4166431ad3703b7c960d5b4d2581fb7bce26cb3f
PandaFou87 <delacote.cedric@gmx.fr>
2025-12-26 00:48:18 +0100

    Fix Discord animated avatar support

--- src/routes/me.js
+++ src/routes/me.js
@@ -2,15 +2,22 @@ const express = require('express');
 const router = express.Router();
 const authJWT = require('../middlewares/authJWT');
 
-router.get('/', authJWT, async (req, res) => {
-  const { user } = req; // injecté par authJWT
+router.get('/', authJWT, (req, res) => {
+  const user = req.user;
+
+  let avatarUrl = null;
+
+  if (user.avatar) {
+    const isAnimated = user.avatar.startsWith('a_');
+    const extension = isAnimated ? 'gif' : 'png';
+
+    avatarUrl = `https://cdn.discordapp.com/avatars/${user.id}/${user.avatar}.${extension}`;
+  }
 
   res.json({
     id: user.id,
     username: user.global_name || user.username,
-    avatar: user.avatar
-      ? `https://cdn.discordapp.com/avatars/${user.id}/${user.avatar}.png`
-      : null
+    avatar: avatarUrl
   });
 });
 
